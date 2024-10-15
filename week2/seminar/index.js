372af9d8ba58eb5bf8e63e21928ebc59f3f38d90
yarimu <yhli0320@naver.com>
2024-10-15 16:22:05 +0900

    feat: 투두 삭제 버튼, 삭제 기능

--- week2/seminar/index.js
+++ week2/seminar/index.js
@@ -1,20 +1,30 @@
+// 요소 선택
 const todoInput = document.querySelector("#input");
 const todo = document.querySelector(".todo_list");
 const submitBtn = document.querySelector(".add_btn");
-
-function paintToDo(toDo) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerHTML = toDo;
-  li.appendChild(span);
-  todo.appendChild(li);
-}
 
 function createToDo(event) {
   event.preventDefault();
   const toDo = todoInput.value;
   paintToDo(toDo);
   todoInput.value = "";
+}
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  li.appendChild(span);
+  li.appendChild(button);
+  span.innerHTML = newToDo;
+  button.innerHTML = "X";
+  button.addEventListener("click", deleteTodo);
+  todo.appendChild(li);
+}
+
+function deleteTodo(event) {
+  const removeTodoItem = event.target.parentElement;
+  removeTodoItem.remove();
 }
 
 function init() {
