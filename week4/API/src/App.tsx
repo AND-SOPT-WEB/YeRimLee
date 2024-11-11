97b8ae0faac6f29e6cc853518e710d0a7fbd52f8
yarimu <yhli0320@naver.com>
2024-11-12 05:29:03 +0900

    init: 라우터 초기세팅

--- week4/API/src/App.tsx
+++ week4/API/src/App.tsx
@@ -1,21 +1,25 @@
 import { Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./styles/theme";
 import globalStyles from "./styles/global";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Join from "./pages/Join";
 import Login from "./pages/Login";
 import MyPage from "./pages/MyPage";
 
 function App() {
   return (
-    <>
+    <Router>
       <ThemeProvider theme={theme}>
         <Global styles={globalStyles} />
 
-        <Join />
-        <Login />
-        <MyPage />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
       </ThemeProvider>
-    </>
+    </Router>
   );
 }
 
