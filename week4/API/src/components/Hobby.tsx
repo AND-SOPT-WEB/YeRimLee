2a9f25d7ba5805283d69c5b56329e5601c7a0d5c
yarimu <yhli0320@naver.com>
2024-11-12 04:19:50 +0900

    design: 취미 컴포넌트 퍼블리싱

---
+++ week4/API/src/components/Hobby.tsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+import Title from "./Title";
+import Input from "./Input";
+import Button from "./Button";
+
+const Hobby = () => {
+  return (
+    <Container>
+      <FormLayout>
+        <Title title="취미" />
+        <Title title="나의 취미" fontSize="medium" />
+        <Title title="독서" fontSize="small" />
+        <Title title="다른 사람들의 취미" fontSize="medium" />
+        <Input text="사용자 번호" />
+        <Button text="검색" />
+      </FormLayout>
+    </Container>
+  );
+};
+
+export default Hobby;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const FormLayout = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40rem;
+  padding: 3rem;
+  border-radius: 10px;
+`;
