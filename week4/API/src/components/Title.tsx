392375fd9096e1c61fc8e25bdae51babd6c00f24
yarimu <yhli0320@naver.com>
2024-11-12 03:58:05 +0900

    design: 회원가입 퍼블리싱

--- week4/API/src/components/Title.tsx
+++ week4/API/src/components/Title.tsx
@@ -14,4 +14,6 @@ export default Title;
 const StyledTitle = styled.h2<{ fontSize: "large" | "medium" }>`
   font-size: ${({ theme, fontSize }) => theme.fontSize[fontSize]};
   margin-bottom: ${({ fontSize }) => (fontSize === "large" ? "2rem" : "1rem")};
+  align-self: ${({ fontSize }) =>
+    fontSize === "medium" ? "flex-start" : "center"};
 `;
--- week4/API/src/pages/Join.tsx
+++ week4/API/src/pages/Join.tsx
@@ -1,5 +1,36 @@
+import styled from "@emotion/styled";
+import Title from "../components/Title";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import SmallLink from "../components/SmallLink";
+
 const Join = () => {
-  return <div>Join컴포</div>;
+  return (
+    <Container>
+      <FormLayout>
+        <Title title="회원가입" />
+        <Title title="아이디" fontSize="medium" />
+        <Input text="사용자 이름을 입력해 주세요" />
+        <Button text="다음" />
+        <SmallLink text="로그인" showDesc={true} />
+      </FormLayout>
+    </Container>
+  );
 };
 
 export default Join;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const FormLayout = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40rem;
+  padding: 3rem;
+  border-radius: 10px;
+`;
+
