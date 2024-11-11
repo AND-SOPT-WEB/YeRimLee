b77c75f91975f6510a503d5d4199d4335ba2365d
yarimu <yhli0320@naver.com>
2024-11-12 03:39:57 +0900

    chore: 불필요한 중괄호 제거

--- week4/API/src/pages/Login.tsx
+++ week4/API/src/pages/Login.tsx
@@ -8,11 +8,11 @@ const Login = () => {
   return (
     <Container>
       <FormLayout>
-        <Title title={"로그인"} />
-        <Input text={"아이디"} />
-        <Input text={"비밀번호"} />
-        <Button text={"로그인"} />
-        <SmallLink text={"회원가입"} />
+        <Title title="로그인" />
+        <Input text="아이디" />
+        <Input text="비밀번호" />
+        <Button text="로그인" />
+        <SmallLink text="회원가입" />
       </FormLayout>
     </Container>
   );
