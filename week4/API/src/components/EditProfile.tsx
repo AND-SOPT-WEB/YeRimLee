f0b59bb38a3062d3ab624a5968a9d43fd444970e
yarimu <yhli0320@naver.com>
2024-11-12 04:26:07 +0900

    design: 내 정보 수정하기 컴포 퍼블리싱

---
+++ week4/API/src/components/EditProfile.tsx
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled";
+import Title from "./Title";
+import Input from "./Input";
+import Button from "./Button";
+
+const EditProfile = () => {
+  return (
+    <Container>
+      <FormLayout>
+        <Title title="내 정보 수정하기" />
+        <Title title="새 비밀번호" fontSize="medium" />
+        <Input />
+
+        <Title title="새 취미" fontSize="medium" />
+        <Input />
+        <Button text="수정하기" />
+      </FormLayout>
+    </Container>
+  );
+};
+
+export default EditProfile;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const FormLayout = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40rem;
+  padding: 3rem;
+  border-radius: 10px;
+`;
--- week4/API/src/components/Input.tsx
+++ week4/API/src/components/Input.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 interface InputProps {
-  text: string;
+  text?: string;
 }
 
 const Input = ({ text }: InputProps) => {
