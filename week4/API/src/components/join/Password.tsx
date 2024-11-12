a83f1cec0557367130992ec6c287f52a00d18412
yarimu <yhli0320@naver.com>
2024-11-12 21:15:24 +0900

    feat: 회원가입(비밀번호) 비활성화

--- week4/API/src/components/join/Password.tsx
+++ week4/API/src/components/join/Password.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Title from "../common/Title";
 import Input from "../common/Input";
 import Button from "../common/Button";
@@ -8,15 +9,33 @@ interface HobbyProps {
 }
 
 const Password = ({ handleNextStep, getUserPw }: HobbyProps) => {
+  const [userPw, setUserPw] = useState("");
+  const [confirmPw, setConfirmPw] = useState("");
+  const isPwFieldsFull = userPw && confirmPw;
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     getUserPw(e.target.value);
+    setUserPw(e.target.value);
+  };
+
+  const handleConfirmChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    getUserPw(e.target.value);
+    setConfirmPw(e.target.value);
   };
   return (
     <>
       <Title title="비밀번호" fontSize="medium" />
-      <Input text="비밀번호를 입력해 주세요" onChange={handleChange} />
-      <Input text="비밀번호 확인" onChange={handleChange} />
-      <Button text="다음" onClick={handleNextStep} />
+      <Input
+        text="비밀번호를 입력해 주세요"
+        value={userPw}
+        onChange={handleChange}
+      />
+      <Input
+        text="비밀번호 확인"
+        value={confirmPw}
+        onChange={handleConfirmChange}
+      />
+      <Button text="다음" onClick={handleNextStep} disabled={!isPwFieldsFull} />
     </>
   );
 };
