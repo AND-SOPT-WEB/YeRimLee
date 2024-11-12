ef325201f7356e33aa72c40c1c46ed3a8fae449f
yarimu <yhli0320@naver.com>
2024-11-12 22:47:54 +0900

    feat: 회원가입(비밀번호) 에러메시지 출력

--- week4/API/src/components/join/Password.tsx
+++ week4/API/src/components/join/Password.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Title from "../common/Title";
 import Input from "../common/Input";
 import Button from "../common/Button";
+import styled from "@emotion/styled";
 
 interface HobbyProps {
   handleNextStep: () => void;
@@ -13,9 +14,11 @@ const Password = ({ handleNextStep, getUserPw }: HobbyProps) => {
   const [confirmPw, setConfirmPw] = useState("");
 
   const isPasswordValid = userPw && confirmPw && userPw === confirmPw;
-  const isPasswordLengthValid = userPw && userPw.length < 8;
+  const isPasswordLengthValid = userPw.length < 8;
+  const isConfirmPwLengthValid = confirmPw.length < 8;
 
-  const pwBtnActive = isPasswordValid && isPasswordLengthValid;
+  const pwBtnActive =
+    isPasswordValid && isPasswordLengthValid && isConfirmPwLengthValid;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     getUserPw(e.target.value);
@@ -40,9 +43,26 @@ const Password = ({ handleNextStep, getUserPw }: HobbyProps) => {
         value={confirmPw}
         onChange={handleConfirmChange}
       />
+
+      {/* 비밀번호가 8자 이상일 경우 에러 메시지 */}
+      {userPw.length >= 8 && (
+        <ErrorMessage>비밀번호는 8자 이내로 입력해주세요.</ErrorMessage>
+      )}
+
+      {/* 비밀번호 불일치 에러 메시지 */}
+      {!isPasswordValid && confirmPw.length > 0 && (
+        <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
+      )}
+
       <Button text="다음" onClick={handleNextStep} disabled={!pwBtnActive} />
     </>
   );
 };
+
+const ErrorMessage = styled.div`
+  color: ${({ theme }) => theme.colors.error};
+  font-size: ${({ theme }) => theme.fontSize.small};
+  align-self: flex-start;
+`;
 
 export default Password;
